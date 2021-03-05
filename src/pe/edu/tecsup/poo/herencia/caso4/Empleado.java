375d7a1fb470f2a09466aa6a36e078542add352f
usuario01 <usuario01@gmail.com>
2021-03-04 21:17:38 -0500

    Update

--- src/pe/edu/tecsup/poo/herencia/caso4/Empleado.java
+++ src/pe/edu/tecsup/poo/herencia/caso4/Empleado.java
@@ -7,11 +7,18 @@ package pe.edu.tecsup.poo.herencia.caso4;
  */
 public abstract class Empleado extends Trabajador {
 
+	protected int nroSeguroSocial;
 	
 	/**
 	 * Definiendo un comportamiento sin implementar : metodo abstracto
 	 * @return
 	 */
 	public abstract int calcularHorasDeTrabajo();
+	
+	
+	public void imprimirSaludo() {
+		
+	}
+	
 	
 }
